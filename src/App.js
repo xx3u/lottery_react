3ceac23317a8c0b22a1a91a50490861201e50dab
YK <yuliya.khv@gmail.com>
2020-12-24 23:30:28 +0600

    #1  Создан компонент Number, подключен к App, создана функция randomNumber с генерацией случайного числа

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, {Component} from 'react';
+import Number from './Number/Number';
+
+class App extends Component {
+  state = {
+    numbers: [
+      {value: null}
+    ]
+  };
+
+  randomNumber = () => {
+    const random = Math.floor(Math.random() * 36 + 1);
+
+    this.setState({
+      numbers: [
+        {value: random}
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.randomNumber}>New numbers</button>
+        <Number value={this.state.numbers[0].value}/>
+      </div>
+    );
+  };    
 }
 
 export default App;
